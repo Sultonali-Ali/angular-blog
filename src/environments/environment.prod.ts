3fb150eba5fbd33ea103c678b65620d8b772fefc
Jalolov Sultonali <jsultonali1993@mail.ru>
2020-02-01 11:41:09 +0500

    Finished ch10: Setting Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyC1Ot1WgD55cCq5WItDgxW7d6PjBjwUvlg'
 };
