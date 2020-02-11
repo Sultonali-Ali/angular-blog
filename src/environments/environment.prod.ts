5c83000e48fcfe02ba2c1dfbce42066bfcd655e6
Jalolov Sultonali <jsultonali1993@mail.ru>
2020-02-11 13:05:53 +0500

    Finished ch16: update to github

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyC1Ot1WgD55cCq5WItDgxW7d6PjBjwUvlg'
+  apiKey: 'AIzaSyC1Ot1WgD55cCq5WItDgxW7d6PjBjwUvlg',
+  fbDbUrl: 'https://angular-blog-c7a1b.firebaseio.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyC1Ot1WgD55cCq5WItDgxW7d6PjBjwUvlg'
+  apiKey: 'AIzaSyC1Ot1WgD55cCq5WItDgxW7d6PjBjwUvlg',
+  fbDbUrl: 'https://angular-blog-c7a1b.firebaseio.com'
 };
 
 /*
